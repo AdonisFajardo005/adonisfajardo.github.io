531e510a2bb8e497766579bf928ee01d6e98f725
Adonis Fajardo <adonis.fajardo@outlook.es>
2026-03-10 18:42:05 -0600

    datos

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
-function toggleMenu(){
-document.getElementById("nav").classList.toggle("show");
+function goToGithub(){
+
+window.open(
+"https://github.com/AdonisFajardo005",
+"_blank"
+);
+
 }
 
+
+/* animación scroll */
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("visible")
+
+}
+
+})
+
+})
+
+document.querySelectorAll(".fade-in").forEach(el=>{
+
+observer.observe(el)
+
+})
+
+
+/* repositorios github */
+
 fetch("https://api.github.com/users/AdonisFajardo005/repos")
-.then(res => res.json())
-.then(data => {
 
-const repos = document.getElementById("repos")
+.then(res=>res.json())
 
-data.slice(0,6).forEach(repo => {
+.then(data=>{
 
-const div = document.createElement("div")
+const repos=document.getElementById("repos")
 
-div.className="repo"
+data.slice(0,6).forEach(repo=>{
 
-div.innerHTML=`
+const card=document.createElement("div")
+
+card.className="repo-card"
+
+card.innerHTML=`
 
 <h3>${repo.name}</h3>
 
-<p>${repo.description || "Proyecto de software"}</p>
+<p>${repo.description || "Proyecto de desarrollo"}</p>
 
-<a href="${repo.html_url}" target="_blank">Ver repositorio</a>
+<a href="${repo.html_url}" target="_blank">
+Ver repositorio
+</a>
 
 `
 
-repos.appendChild(div)
+repos.appendChild(card)
 
 })
 
 })
+
